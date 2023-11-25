520f61c32c2f1fac4097f2bf30e014a98e0e824d
hashiotoko <141216166+hashiotoko@users.noreply.github.com>
2023-11-25 12:30:25 +0900

    remove todo comments

--- packages/backend/src/resolvers/queries/members.ts
+++ packages/backend/src/resolvers/queries/members.ts
@@ -10,7 +10,6 @@ import {
 } from '../../utils/pagination';
 import { camelCase } from 'lodash';
 
-// TODO: 汎用的な処理の抽出
 export const members: GQLQueryResolvers['members'] = async (
   _parent,
   { first, after, last: last, before, filter, orderBy },
@@ -21,7 +20,6 @@ export const members: GQLQueryResolvers['members'] = async (
   const whereConditions: { [key: string]: unknown } =
     Object.keys(filter || {}).length === 0 ? {} : (filter as GQLMemberFilter);
 
-  // TODO: インデックスシグネチャ使わずにエンティティの属性に限定したい
   const order: { -readonly [key in keyof Partial<Member>]: GQLOrderDirection } =
     {};
   if (orderBy != null)
